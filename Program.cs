f6cbe8a0a050dbd8337c78ae4e6bf3cf486fba6d
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 22:24:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FlowerFactory
+{
+    private Dictionary<string, string> flowers = new Dictionary<string, string>();
+
+    public string GetFlower(string type)
+    {
+        if (!flowers.ContainsKey(type))
+        {
+            flowers[type] = $"Створено новий об’єкт: {type}";
+        }
+        return flowers[type];
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var factory = new FlowerFactory();
+
+        Console.WriteLine(factory.GetFlower("Троянда"));
+        Console.WriteLine(factory.GetFlower("Троянда")); // Використовує існуючий
+    }
+}
+
+
+
+
+
+
+
+
